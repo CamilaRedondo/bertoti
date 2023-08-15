172919996622e806764bd5256943b2390ab78776
Camila Redondo <98497251+CamilaRedondo@users.noreply.github.com>
2023-08-15 11:02:59 -0300

    Add files via upload

---
+++ engenhariaIII/strategy/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Quadrilatero figura01 = new Quadrilatero();
+        figura01.setArea(new Quadrado());
+        figura01.fazerCalculo();
+    }
+}
---
+++ engenhariaIII/strategy/src/Trapezio.java
@@ -0,0 +1,7 @@
+public class Trapezio implements Area{
+    @Override
+    public void calcular() {
+        System.out.println("AreaTrapezio = (BaseMaior + BaseMenor) * altura / 2");
+        // area = (a + b) * h / 2
+    }
+}
